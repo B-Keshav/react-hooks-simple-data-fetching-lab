e8d61da0eb6de9d57784102efe9a3200be640bce
Keshav Bhati <keshavbhati2000@gmail.com>
2022-10-18 12:01:52 -0400

    commit tree 5e7704e9586b1c1f5d4f0ade37df426944d1805e

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
+import { useEffect, useState } from "react"
+
 // create your App component here
+function App(){
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [image, setImage]= useState("")
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            console.log("use effect used")
+            setImage(data.message)
+            setIsLoaded(true)
+        })
+    },[])
+    
+    
+    if(!isLoaded){
+        return <p>Loading...</p>
+    }
+
+    return(
+        <img src={image} alt="A Random Dog" />
+    )
+
+}
+export default App
